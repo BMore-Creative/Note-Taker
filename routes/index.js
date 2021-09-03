6421bbfe729dfaf61c33bf31a40cd79c88a70237
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-09-03 12:51:50 -0700

    consolidated routes into single js file to work with provided frontend js

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const path = require('path')
+const db = require('../db/db.json')
+
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+
+router.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
+
+router.get('/api/notes', (req, res) => res.json(db))
+
+router.post('/api/notes', (req, res) => db.push(req.body))
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const apiRoutes = require('./apiRoutes')
-const htmlRoutes = require('./htmlRoutes')
+const express = require("express");
+const path = require("path");
+const routes = require('./routes')
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
-
-app.use
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', htmlRoutes)
-app.use('/api', apiRoutes)
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
